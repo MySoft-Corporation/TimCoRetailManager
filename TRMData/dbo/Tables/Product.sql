069894e18a28fb90d9f186ba4f23010867d9a146
Ali Ahmad <ahmadonline@outlook.com>
2020-08-26 08:33:26 +0500

    Register DB Tables

---
+++ TRMData/dbo/Tables/Product.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [BarCode] INT NULL, 
+    [Name] NCHAR(100) NULL, 
+    [Description] NVARCHAR(MAX) NULL, 
+    [CreateDate] DATETIME2 NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NULL
+)
---
+++ TRMData/dbo/Tables/SaleDetail.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SaleDetail]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [SaleId] NCHAR(10) NULL, 
+    [ProductId] INT NULL, 
+    [Quantity] INT NULL, 
+    [PurchasePrice] MONEY NULL, 
+    [Tax] MONEY NULL
+)
